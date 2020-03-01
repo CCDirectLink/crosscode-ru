18fb5713ffc0218c8e3a311b97111ca979622fad
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-03-01 15:47:23 +0200

    add partial fix for sc.InfoBar

--- mod/src/info-bar.js
+++ mod/src/info-bar.js
@@ -11,11 +11,12 @@ ig.module('crosscode-ru.fixes.info-bar')
         if (width == null) width = ig.system.width;
         if (height == null) height = 21;
         if (skipRender == null) skipRender = false;
+
         ig.GuiElementBase.prototype.init.call(this);
-        console.log(width, height);
         this.setSize(width, height);
         this.setStateValue('HIDDEN', 'offsetY', -this.hook.size.y);
         this.skipRender = skipRender;
+        // this is the only changed line:
         this.text = new sc.ru.LongHorizontalTextGui('');
         this.text.setAlign(ig.GUI_ALIGN.X_RIGHT, ig.GUI_ALIGN.Y_CENTER);
         this.text.setPos(8, 0);
@@ -31,6 +32,32 @@ ig.module('crosscode-ru.fixes.info-bar')
         this.text.doStateTransition('HIDDEN', true);
         this.addChildGui(this.text);
         this.doStateTransition('HIDDEN', true);
+
+        this._updateTickerConfig();
+      },
+
+      update() {
+        let shouldUpdateConfig = this.sizeTransition != null;
+        this.parent();
+        if (shouldUpdateConfig) this._updateTickerConfig();
+      },
+
+      doSizeTransition(width, height, time, timeFunction, delay) {
+        this.parent(width, height, time, timeFunction, delay);
+        if (time == null) this._updateTickerConfig();
+      },
+
+      _updateTickerConfig() {
+        this.text.setTickerConfig({
+          maxSize: { x: this.hook.size.x - this.text.hook.pos.x * 2 },
+        });
+      },
+
+      updateDrawables(renderer) {
+        this.parent(renderer);
+        renderer
+          .addColor('blue', 0, 0, this.hook.size.x, this.hook.size.y)
+          .setAlpha(0.1);
       },
     });
   });
