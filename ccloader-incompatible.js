99ea81223045713da563fa37b70290841d2bd94e
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-03-22 17:50:20 +0200

    make the mod compatible with CCLoader again

--- ccloader-incompatible.js
+++
@@ -1,4 +0,0 @@
-const ERROR =
-  "crosscode-ru-ng is incompatible with the main branch of CCLoader. I'm waiting for my ccmod.json proposal to be accepted. In the meantime you can use the branch ccmod-json-manifest from my fork of CCLoader: https://github.com/dmitmel/CCLoader/tree/ccmod-json-manifest";
-alert(ERROR);
-throw new Error(ERROR);
