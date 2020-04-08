7cde46f56e36c34a32b8f491d7cf7d8447108e87
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-04-08 19:43:22 +0300

    translate to odd labels in the Linde quest

--- src/json-patches.js
+++ src/json-patches.js
@@ -81,6 +81,18 @@ const PATCHES = {
   'data/lang/sc/gui.ko_KR.json': addEnglishLabelsToLangFile,
   'data/lang/sc/gui.zh_CN.json': addEnglishLabelsToLangFile,
 
+  'data/maps/jungle/grove/grove-path-02.json': data => {
+    sc.ru.localizeProp(data.entities[892].settings.event[0], 'value', {
+      orig: '[Inspecting]',
+      text: '[Изучает]',
+    });
+    sc.ru.localizeProp(data.entities[892].settings.event[1], 'value', {
+      orig: '[Escort Linde here]',
+      text: '[Сопровождайте Линдэ]',
+    });
+    return data;
+  },
+
   // sorry, Felix... not many people watch credits until the end, so I'll have
   // to inject our names in the first data file. hope RFG doesn't mind :P
   'data/credits/radicalfish-core.json': async data => {
