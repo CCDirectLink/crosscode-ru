8de12fbdd478af090f8d667ad5f90dcd3a8c917a
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-04-15 17:11:43 +0300

    move everything to postload

--- src/postload.ts
+++ src/postload.ts
@@ -3,5 +3,28 @@ import './debug.js';
 import './utils.js';
 import '../src/locale.js';
 import './localize-me-ready-module.js';
+
 import './json-patches.js';
 import './image-patches.js';
+
+import './localization-utils.js';
+
+import './arena-gui.js';
+import './circuit-menu.js';
+import './credits-section.js';
+import './event-steps-patches.js';
+import './help.js';
+import './level-up.js';
+import './main-menu.js';
+import './map-menu.js';
+import './options-menu.js';
+import './pause-screen.js';
+import './pvp.js';
+import './ru-social-button.js';
+import './saves-menu.js';
+import './social-menu.js';
+import './status-combat-arts.js';
+import './status-lines.js';
+import './tab-button.js';
+import './title-screen.js';
+import './traders-list.js';
