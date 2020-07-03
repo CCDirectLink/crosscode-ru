206912397ce2a6cea133b83572e8601b8be63dcf
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-07-03 12:17:07 +0300

    close the tool window when the game window is closed (finally)
    this is possible now with ccloader3 because it doesn't run game's
    node-webkit.html an iframe

--- tool/src/mod/tool-client.ts
+++ tool/src/mod/tool-client.ts
@@ -1,18 +1,15 @@
 import { Settings, readSettings } from '../settings.js';
 
-let scriptUrl: string = import.meta.url;
-let scriptDir = scriptUrl.slice(0, scriptUrl.lastIndexOf('/'));
+const TOOL_HTML_PAGE_URL: URL = new URL('../../main.html', import.meta.url);
 
 export default class TranslationToolClient {
-  // private gameWindow: nw.Window = nw.Window.get();
+  private gameWindow: nw.Window = nw.Window.get();
   private toolWindow: nw.Window | null = null;
   private toolWindowIsOpening = false;
 
-  // TODO: enable this when I find a workaround for listening to window events
-  // in the game iframe
-  // constructor() {
-  //   this.gameWindow.on('close', this.onGameWindowClose.bind(this));
-  // }
+  public constructor() {
+    this.gameWindow.on('close', this.onGameWindowClose.bind(this));
+  }
 
   public readSettings(): Promise<Settings> {
     return readSettings();
@@ -26,26 +23,23 @@ export default class TranslationToolClient {
     }
 
     this.toolWindowIsOpening = true;
-    nw.Window.open(
-      `${scriptDir}/../../main.html`,
-      // magic values from the game's package.json
-      { width: 1136, height: 640 },
-      (toolWindow) => {
-        if (toolWindow == null) return;
+    let windowOpenOptions: nw.Window.Manifest = { ...nw.App.manifest.window };
+    delete windowOpenOptions.toolbar;
+    nw.Window.open(TOOL_HTML_PAGE_URL.href, windowOpenOptions, (toolWindow) => {
+      if (toolWindow == null) return;
 
-        this.toolWindow = toolWindow;
-        this.toolWindowIsOpening = false;
+      this.toolWindow = toolWindow;
+      this.toolWindowIsOpening = false;
 
-        // (this.toolWindow.window as any).gameWindow = this.gameWindow;
-        this.toolWindow.on('closed', () => {
-          this.toolWindow = null;
-        });
-      },
-    );
+      // (this.toolWindow.window as any).gameWindow = this.gameWindow;
+      this.toolWindow.on('closed', () => {
+        this.toolWindow = null;
+      });
+    });
   }
 
-  // private onGameWindowClose(): void {
-  //   if (this.toolWindow != null) this.toolWindow.close();
-  //   this.gameWindow.close(true);
-  // }
+  private onGameWindowClose(): void {
+    if (this.toolWindow != null) this.toolWindow.close();
+    this.gameWindow.close(true);
+  }
 }
