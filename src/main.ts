9aaff3ba31c16db250b6bef23ef172be314263d2
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-07-25 14:00:15 +0300

    mark the main class as an implementor of modloader.Mod.MainClass

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-export default class {
+export default class implements modloader.Mod.MainClass {
   public postload(mod: modloader.Mod): void {
     if (sc.ru == null) sc.ru = {} as typeof sc.ru;
     sc.ru.version = mod.version.toString();
