2d06257d5465ac130afdda8fa659898e4de8a4cc
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-08-18 01:04:35 +0300

    upgrade to the latest typedefs version

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 export default class implements modloader.Mod.MainClass {
   public postload(mod: modloader.Mod): void {
     if (sc.ru == null) sc.ru = {} as typeof sc.ru;
-    sc.ru.version = mod.version.toString();
+    sc.ru.version = mod.version!.toString();
   }
 }
