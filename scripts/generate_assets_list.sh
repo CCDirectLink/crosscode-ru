57a4ba0aaeab5254083dba1fb8ad7b4946c6626b
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-11-20 18:58:38 +0200

    add a list of the asset overrides for browser support

---
+++ scripts/generate_assets_list.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env sh
+set -eu
+# I'd like to know why is --null-input necessary.
+# <https://github.com/stedolan/jq/issues/822#issuecomment-113655116>
+find assets -type f -not -name '*.xcf' -printf '%P\n' | jq --raw-input --null-input '[inputs] | sort'
