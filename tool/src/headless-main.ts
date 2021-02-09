aad772b5af52d0daf378d3a3ded793383538b1cb
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-02-09 22:04:24 +0200

    compile the notabenoid client for node.js, prepare to implement a headless downloader

---
+++ tool/src/headless-main.ts
@@ -0,0 +1,11 @@
+import { NotaClient } from './Notabenoid';
+
+async function main(): Promise<void> {
+  try {
+  } catch (err) {
+    console.error(err);
+    process.exitCode = 1;
+  }
+}
+
+void main();
