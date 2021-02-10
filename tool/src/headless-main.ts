470b7de06b9b3097bb90949f88cc73d2d2a642cc
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-02-10 13:45:05 +0200

    implement a CLI for the headless tool

--- tool/src/headless-main.ts
+++ tool/src/headless-main.ts
@@ -5,9 +5,67 @@ import { NodejsNotaHttpClient } from './Notabenoid/nodejs';
 
 import * as asyncUtils from './utils/async.js';
 
+import * as fs from 'fs';
+import * as yargs from 'yargs';
+
+interface CliOptions {
+  username: string;
+  password: string;
+  output: string;
+  force: boolean;
+}
+
+function parseCliOptions(): CliOptions {
+  function loadSecretOption(arg: unknown): string {
+    let argStr = String(arg);
+    if (argStr.startsWith('@')) {
+      return argStr.slice(1);
+    } else {
+      return fs.readFileSync(argStr, 'utf8').trim();
+    }
+  }
+
+  return yargs(process.argv.slice(2))
+    .usage('$0')
+    .options({
+      username: {
+        string: true,
+        requiresArg: true,
+        alias: 'u',
+        demandOption: true,
+        coerce: loadSecretOption,
+        description: 'Notabenoid.org username',
+      },
+      password: {
+        string: true,
+        requiresArg: true,
+        alias: 'p',
+        demandOption: true,
+        coerce: loadSecretOption,
+        description: 'Notabenoid.org password',
+      },
+      output: {
+        string: true,
+        requiresArg: true,
+        alias: 'o',
+        demandOption: true,
+        description: 'Path to the local database dir',
+      },
+      force: {
+        alias: 'f',
+        boolean: true,
+        default: false,
+        description: 'Whether to download all chapters even if some were unchanged',
+      },
+    })
+    .help().argv;
+}
+
 async function main(): Promise<void> {
+  let opts = parseCliOptions();
+
   let notaClient = new NotaClient(new NodejsNotaHttpClient());
-  await notaClient.login('username', 'password');
+  await notaClient.login(opts.username, opts.password);
 
   let chapterStatuses: Map<string, ChapterStatus> = await notaClient.fetchAllChapterStatuses();
   let chapterFragments = new Map<string, Fragment[]>();
