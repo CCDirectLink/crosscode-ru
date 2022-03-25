e3a3f86c9b25a142ab0c6d6e6fa7ed9b29f83328
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-03-25 12:27:53 +0200

    move the only script in tool/scripts/ to the common scripts/ directory

--- scripts/generate_node_builtin_module_crutches.cjs
+++ scripts/generate_node_builtin_module_crutches.cjs
@@ -3,7 +3,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const OUTPUT_DIR = path.join(__dirname, '..', 'src', 'node-builtin-modules');
+const OUTPUT_DIR = path.join(__dirname, '..', 'tool', 'src', 'node-builtin-modules');
 const MODULES = ['fs', 'path', 'child_process'];
 
 fs.mkdirSync(OUTPUT_DIR, { recursive: true });
